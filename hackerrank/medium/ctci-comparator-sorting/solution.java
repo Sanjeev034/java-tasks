43c6f6f42724e892b03e74e3690aa6dda66e1cc4
Sanjeev034 <vtu27899@veltech.edu.in>
2026-08-25 10:14:59 +0530

    ✅ ctci-comparator-sorting | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ hackerrank/medium/ctci-comparator-sorting/solution.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Checker implements Comparator<Player> {
+
+    @Override
+    public int compare(Player a, Player b) {
+
+        // Sort by score in descending order
+        if (a.score != b.score) {
+            return b.score - a.score;
+        }
+
+        // If scores are equal, sort by name
+        // in alphabetical order
+        return a.name.compareTo(b.name);
+    }
+}
+
+class Player {
+
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        Player[] players = new Player[n];
+
+        for (int i = 0; i < n; i++) {
+            players[i] = new Player(
+                scan.next(),
+                scan.nextInt()
+            );
+        }
+
+        Checker checker = new Checker();
+
+        Arrays.sort(players, checker);
+
+        for (Player player : players) {
+            System.out.println(
+                player.name + " " + player.score
+            );
+        }
+
+        scan.close();
+    }
+}
