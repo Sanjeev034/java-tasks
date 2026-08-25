8c209b2476defa8a16b689e93d7de2ecf9476975
Sanjeev034 <vtu27899@veltech.edu.in>
2026-08-25 10:13:11 +0530

    Force committed solution

--- 13817-java-comparator-Hackerrank/13817-java-comparator.java
+++ 13817-java-comparator-Hackerrank/13817-java-comparator.java
@@ -5,17 +5,16 @@ class Checker implements Comparator<Player> {
     @Override
     public int compare(Player a, Player b) {
 
-        // Higher score first
         if (a.score != b.score) {
             return b.score - a.score;
         }
 
-        // Same score -> alphabetical order
         return a.name.compareTo(b.name);
     }
 }
 
 class Player {
+
     String name;
     int score;
 
